a41f2406640a2490a4019904cb78060f906e3a78
Chintalagiri Shashank <shashank@chintal.in>
2015-10-06 11:42:40 +0530

    Bumped to 0.2.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='tendril-fs-server',
-    version='0.2.0',
+    version='0.2.1',
     description="XML-RPC Filesystem Server using Twisted and Pyfilesystems for Tendril",
     long_description=readme,
     author="Chintalagiri Shashank",
